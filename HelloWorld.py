81608db6bb849e0fdd5bc3db2d2d85b9a11effc8
PrpgramTraveler <2115655391@qq.com>
2020-12-19 11:53:53 +0800

    2020-12-19

---
+++ HelloWorld.py
@@ -0,0 +1,25 @@
+# 单行注释 ctl + /
+
+"""
+多行注释，三对单引号或者三对双引号
+"""
+
+# 输出
+print("hello world")
+# 运行这个文件 右键单击文件 然后run
+
+
+"""
+1.Python 环境
+    安装 --- 第一个页面 Add Python to path [选中]
+    把Python环境加到环境变量中path的用处：
+        可以任意目录下使用Python环境
+    注意点：
+        安装Python环境的目录需要一个单独的空白文件
+        安装忘记点击 Add -> 不是内部或者外部命令 -> 把Python.exe和pip.exe放入路径中
+2.PythonCharm 集成开发工具
+    设置这个工具的Python开发环境的位置
+        configure -> setting -> Python Interpreter / Project
+    或者 项目创建完成了怎么设置环境
+        File -> Setting -> Project -> python Interpreter -> 选择existing environ
+"""
