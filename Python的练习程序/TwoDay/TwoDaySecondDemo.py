7246a3f4994f065d827ff26dba39e9e0c58c2744
PrpgramTraveler <2115655391@qq.com>
2020-12-20 18:51:07 +0800

    2020-12-20

--- Python的练习程序/TwoDay/TwoDaySecondDemo.py
+++ Python的练习程序/TwoDay/TwoDaySecondDemo.py
@@ -5,9 +5,5 @@ author:王久铭
 # 获得1-100的一组列表
 num = range(1,100)
 # 记录满足条件的数
-res = []
-# 遍历列表
-for tem in num :
-    if tem % 10 == 3 :
-        res.append(tem)
+res = [w for w in num if w % 10 == 3]
 print(res)
